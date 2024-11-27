9e16531d18d4c52ae92245f752c478ab10b9beb2
Jiri Sevcik <jiri.sevcik@primehammer.com>
2024-11-27 09:09:37 +0100

    Update inputs

--- event_stats_client/models/trending_event_price_schema.py
+++ event_stats_client/models/trending_event_price_schema.py
@@ -20,20 +20,17 @@ import json
 from datetime import datetime
 from pydantic import BaseModel, ConfigDict, StrictStr
 from typing import Any, ClassVar, Dict, List
-from event_stats_client.models.event_store_type import EventStoreType
-from event_stats_client.models.input_event_stats_schema import InputEventStatsSchema
 from typing import Optional, Set
 from typing_extensions import Self
 
-class EventStoreStatsRequestSchema(BaseModel):
+class TrendingEventPriceSchema(BaseModel):
     """
-    EventStoreStatsRequestSchema
+    TrendingEventPriceSchema
     """ # noqa: E501
     event_id: StrictStr
-    event_source: EventStoreType
-    event_timestamp: datetime
-    seat_stats: InputEventStatsSchema
-    __properties: ClassVar[List[str]] = ["event_id", "event_source", "event_timestamp", "seat_stats"]
+    timestamp: datetime
+    velocity_change: StrictStr
+    __properties: ClassVar[List[str]] = ["event_id", "timestamp", "velocity_change"]
 
     model_config = ConfigDict(
         populate_by_name=True,
@@ -53,7 +50,7 @@ class EventStoreStatsRequestSchema(BaseModel):
 
     @classmethod
     def from_json(cls, json_str: str) -> Optional[Self]:
-        """Create an instance of EventStoreStatsRequestSchema from a JSON string"""
+        """Create an instance of TrendingEventPriceSchema from a JSON string"""
         return cls.from_dict(json.loads(json_str))
 
     def to_dict(self) -> Dict[str, Any]:
@@ -74,14 +71,11 @@ class EventStoreStatsRequestSchema(BaseModel):
             exclude=excluded_fields,
             exclude_none=True,
         )
-        # override the default output from pydantic by calling `to_dict()` of seat_stats
-        if self.seat_stats:
-            _dict['seat_stats'] = self.seat_stats.to_dict()
         return _dict
 
     @classmethod
     def from_dict(cls, obj: Optional[Dict[str, Any]]) -> Optional[Self]:
-        """Create an instance of EventStoreStatsRequestSchema from a dict"""
+        """Create an instance of TrendingEventPriceSchema from a dict"""
         if obj is None:
             return None
 
@@ -90,9 +84,8 @@ class EventStoreStatsRequestSchema(BaseModel):
 
         _obj = cls.model_validate({
             "event_id": obj.get("event_id"),
-            "event_source": obj.get("event_source"),
-            "event_timestamp": obj.get("event_timestamp"),
-            "seat_stats": InputEventStatsSchema.from_dict(obj["seat_stats"]) if obj.get("seat_stats") is not None else None
+            "timestamp": obj.get("timestamp"),
+            "velocity_change": obj.get("velocity_change")
         })
         return _obj
 
